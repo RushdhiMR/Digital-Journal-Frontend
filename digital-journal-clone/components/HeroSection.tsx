f89a698052846699bb09e7865b6bff4731b37894
RushdhiMR <rushdhiriyaj2005@gmail.com>
2026-07-30 19:58:54 +0530

    style: set sharp rectangle edges and rounded-none across HeroSection images and cards

--- digital-journal-clone/components/HeroSection.tsx
+++ digital-journal-clone/components/HeroSection.tsx
@@ -124,7 +124,7 @@ export default function HeroSection() {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 items-stretch">
         
         {/* LEFT CARD: FEATURED ARTICLE CAROUSEL (~67% GRID WIDTH) */}
-        <div className="lg:col-span-8 bg-white rounded-2xl overflow-hidden flex flex-col lg:flex-row items-stretch h-full">
+        <div className="lg:col-span-8 bg-white rounded-none overflow-hidden flex flex-col lg:flex-row items-stretch h-full">
           
           {/* FLUSH LEFT IMAGE (0 Margins) */}
           <div className="lg:w-[48%] relative min-h-[340px] lg:min-h-[420px] flex-shrink-0 group">
@@ -230,7 +230,7 @@ export default function HeroSection() {
         </div>
 
         {/* RIGHT CARD: TRENDING NOW BOX (~33% GRID WIDTH) */}
-        <div className="lg:col-span-4 bg-white rounded-2xl p-6 flex flex-col justify-between h-full">
+        <div className="lg:col-span-4 bg-white rounded-none p-6 flex flex-col justify-between h-full">
           <div>
             {/* Header */}
             <div className="flex items-center justify-between mb-5 pb-2">
@@ -273,7 +273,7 @@ export default function HeroSection() {
                     </div>
                   </div>
 
-                  <Link href={item.href} className="w-[100px] h-[65px] rounded-lg overflow-hidden bg-gray-900 flex-shrink-0 block group relative">
+                  <Link href={item.href} className="w-[100px] h-[65px] rounded-none overflow-hidden bg-gray-900 flex-shrink-0 block group relative">
                     <img
                       src={item.image}
                       alt={item.title}
